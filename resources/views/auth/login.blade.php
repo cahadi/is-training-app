e1a459a5b75e5883120a34612c141a19a8061f21
cahadi <ap357775462534@gmail.com>
2025-03-19 11:49:31 +1000

    Controllers

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,15 +6,8 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
-            <!--
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div> -->
 
-        <div class="form-group">
+        <div class="form-group mt-4">
             <x-input-label for="username" :value="__('Username')" />
             <x-text-input id="username" class="form-control p_input block mt-1 w-full"
                           type="username"
