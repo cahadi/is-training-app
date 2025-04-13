bc62cae85d244365b07ac9fdce5c2ab8ac58be9f
cahadi <ap357775462534@gmail.com>
2025-04-14 02:20:46 +1000

    V1

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function (){
+    return view('auth.login');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
